b9c052f7cfd1926b7746721ee8da398e27378de6
Marco DiPlacido <marcodiplacido@Marcos-MBP.fios-router.home>
2018-11-17 00:50:09 -0500

    adding game component

---
+++ src/game/game.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class Game extends React.Component {
+    public render() {
+        return (
+            <div>
+                <h3>Play nine</h3>
+            </div>
+        );
+    }
+}
