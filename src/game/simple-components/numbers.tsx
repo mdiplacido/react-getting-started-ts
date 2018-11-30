21610201a2c8ca9a41efa146af0620df5dc0b934
Marco DiPlacido <marcodiplacido@Marcos-MBP.fios-router.home>
2018-11-30 18:57:50 -0500

    switching to functional approach for looping

--- src/game/simple-components/numbers.tsx
+++ src/game/simple-components/numbers.tsx
@@ -4,10 +4,14 @@ export const Numbers = () => {
     return (
         <div className="card text-center">
             <div>
-                <span>1</span>
-                <span className="selected">2</span>
-                <span className="used">3</span>
+                {Numbers.list.map((n, i) => <span key={i}>{n}</span>)}
+
+                {/* <span className="selected">2</span>
+                <span className="used">3</span> */}
             </div>
         </div>
     );
 }
+
+Numbers.list = new Array(9).fill(0).map((_, index) => index + 1);
+
--- src/game/simple-components/stars.tsx
+++ src/game/simple-components/stars.tsx
@@ -4,11 +4,9 @@ import * as React from 'react';
 export const Stars = () => {
     const numberOfStars = 1 + Math.floor(Math.random() * 9);
 
-    const stars = [];
-
-    for (let i = 0; i < numberOfStars; i++) {
-        stars.push(<FontAwesomeIcon icon="stroopwafel" />);
-    }
+    const stars = new Array(numberOfStars)
+        .fill(null)
+        .map((_, i) => <FontAwesomeIcon key={i} icon="stroopwafel" />)
 
     return (
         <div className="col-5">
