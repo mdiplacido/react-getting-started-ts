3f49f5d7d5da3c397ad5c374bb33b603cff14dc2
Marco DiPlacido <marcodiplacido@Marcos-MBP.fios-router.home>
2018-11-30 18:45:01 -0500

    dynamic stars

--- src/game/simple-components/stars.tsx
+++ src/game/simple-components/stars.tsx
@@ -2,12 +2,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import * as React from 'react';
 
 export const Stars = () => {
+    const numberOfStars = 1 + Math.floor(Math.random() * 9);
+
+    const stars = [];
+
+    for (let i = 0; i < numberOfStars; i++) {
+        stars.push(<FontAwesomeIcon icon="stroopwafel" />);
+    }
+
     return (
         <div className="col-5">
-            <FontAwesomeIcon icon="stroopwafel" />
-            <FontAwesomeIcon icon="stroopwafel" />
-            <FontAwesomeIcon icon="stroopwafel" />
-            <FontAwesomeIcon icon="stroopwafel" />
+            {stars}
         </div>
     );
 }
