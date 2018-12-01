d34eaf848f537cdd26b3690673a734083c2f04b6
Marco DiPlacido <marcodiplacido@Marcos-MBP.fios-router.home>
2018-12-01 14:59:30 -0500

    basic game working with error varification

--- src/game/game.tsx
+++ src/game/game.tsx
@@ -7,10 +7,12 @@ import { Stars } from './simple-components/stars';
 export interface IGameState {
     selectedNumbers: number[];
     numberOfStars: number;
+    answerIsCorrect: boolean | null;
 }
 
 export class Game extends React.Component<any, IGameState> {
     public state = {
+        answerIsCorrect: null,
         numberOfStars: 1 + Math.floor(Math.random() * 9),
         selectedNumbers: [2, 4],
     }
@@ -23,15 +25,22 @@ export class Game extends React.Component<any, IGameState> {
         this.setState(prevState => ({ selectedNumbers: prevState.selectedNumbers.filter(c => c !== clickedNumber) }));
     }
 
+    public checkAnswer = () => {
+        this.setState(prev => ({
+            ...prev,
+            answerIsCorrect: prev.numberOfStars === prev.selectedNumbers.reduce((acc, curr) => acc + curr, 0)
+        }));
+    }
+
     public render() {
-        const { numberOfStars, selectedNumbers } = this.state;
+        const { numberOfStars, selectedNumbers, answerIsCorrect } = this.state;
         return (
             <div className="container">
                 <h3>Play nine</h3>
                 <hr />
                 <div className="row">
                     <Stars numberOfStars={numberOfStars} />
-                    <Button selectedNumbers={selectedNumbers} />
+                    <Button selectedNumbers={selectedNumbers} checkAnswer={this.checkAnswer} answerIsCorrect={answerIsCorrect} />
                     <Answer selectedNumbers={selectedNumbers} removeNumber={this.removeNumber} />
                 </div>
                 <br />
--- src/game/simple-components/button.tsx
+++ src/game/simple-components/button.tsx
@@ -2,13 +2,35 @@
 import * as React from 'react';
 
 export interface IButtonProps {
+    answerIsCorrect: boolean | null;
     selectedNumbers: number[];
+    checkAnswer: () => void;
 }
 
 export const Button = (props: IButtonProps) => {
+    let button;
+
+    switch (props.answerIsCorrect) {
+        case true:
+            button =
+                <button className="btn btn-success" onClick={props.checkAnswer}>
+                    <i className="fa fa-check" />
+                </button>
+            break;
+        case false:
+            button =
+                <button className="btn btn-danger" onClick={props.checkAnswer}>
+                    <i className="fa fa-times" />
+                </button>
+            break;
+        default:
+            button = <button onClick={props.checkAnswer} className="btn btn-primary" disabled={props.selectedNumbers.length === 0}>=</button>
+            break;
+    }
+
     return (
         <div className="col-2">
-            <button className="btn btn-primary" disabled={props.selectedNumbers.length === 0}>=</button>
+            {button}
         </div>
     );
 }
